a5ac439fe6e2e43a9bd2b12752532a7b1bbec477
qize <zeng_qiao@yeah.net>
2019-11-27 09:56:01 +0800

    update test.py

--- test.py
+++ test.py
@@ -14,11 +14,8 @@ if __name__ == '__main__':
     df = pd.read_csv('parameters.csv',index_col='name')
     data = df.loc[sys.argv[1]]
     M = 200000.0 if np.isnan(data['M']) else data['M']
-    dens = pcsaft_den(x=np.asarray([1.0]), m=np.asarray([M*0.0352]), s=np.asarray([2.939]), e=np.asarray([229.450]), t=273.15, p=101325, phase='liq')
+    dens = pcsaft_den(x=np.asarray([1.0]), m=np.asarray([M*data['m/M']]), s=np.asarray([data['s']]), e=np.asarray([data['e']]), t=273.15, p=101325, phase='liq')
     #dens mol/m3
     dens *= M/1000000
     print(dens)
     sys.exit()
-    dens = pcsaft_den(x=np.asarray([1.0]), m=np.asarray([18.015*0.06687]), s=np.asarray([2.7927]), e=np.asarray([353.9449]), t=273.15, p=101325, phase='liq')
-    dens *= 18.015/1000000
-    print(dens)
